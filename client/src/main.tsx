91abf11ff7027d9bc02656b1b42cbfe8b98927b7
Kevin Wu <kevin13wu@gmail.com>
2026-05-31 18:23:33 -0400

    End of section 4 - Apply React Router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app/layout/App.tsx";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app/routes/Routes.tsx";
 
 import "./app/layout/styles.css";
 import "@fontsource/roboto/300.css";
@@ -10,6 +11,6 @@ import "@fontsource/roboto/700.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
